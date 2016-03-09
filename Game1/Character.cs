3e838cd3d8e2c099f35eb405a3fe984aefcd7a13
tskp44292901 <steven.kp.tjan@gmail.com>
2016-03-09 12:18:15 +0100

    Commit #1
    First commit and a working structured class

---
+++ Game1/Character.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game1
+{
+    class Character
+    {
+        public string Name;
+        public Texture2D SelfImg;
+        public Texture2D FocusImg;
+        public Vector2 Position;
+        public float Angle;
+
+        public Character()
+        {
+            Position = new Vector2(0, 0);
+            Angle = 0;
+        }
+        public Character(string name, Texture2D image, Texture2D image2, Vector2 position)
+        {
+            Name = name;
+            SelfImg = image;
+            FocusImg = image2;
+            Position = position;
+            Angle = 0;
+        }
+        public Rectangle GetSelfRectangle()
+        {
+            return new Rectangle(0, 0, SelfImg.Width, SelfImg.Height);
+        }
+        public Rectangle GetFocusRectangle()
+        {
+            return new Rectangle(0, 0, FocusImg.Width, FocusImg.Height);
+        }
+        public Vector2 GetSelfCenter()
+        {
+            return new Vector2(SelfImg.Width / 2, SelfImg.Height / 2);
+        }
+
+        public Vector2 GetFocusCenter()
+        {
+            return new Vector2(FocusImg.Width / 2, FocusImg.Height / 2);
+        }
+        public void Move(Keys[] keys)
+        {
+
+        }
+        public void Focus(Keys[] keys)
+        {
+            if (keys.Contains(Keys.LeftShift))
+            {
+                Angle += 0.025f;
+                Angle %= 360;
+            }
+            else
+            {
+                Angle = 0;
+            }
+        }
+        public void Update(Keys[] keys)
+        {
+            Move(keys);
+            Focus(keys);
+        }
+    }
+}
---
+++ Game1/Game1.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game1
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Character character;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+            // TODO: Add your initialization logic here
+            character = new Character("Reimu", this.Content.Load<Texture2D>("Reimu/idle1"), this.Content.Load<Texture2D>("Reimu/focus"), new Vector2(Window.ClientBounds.Width / 2, Window.ClientBounds.Height / 2));
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            character.Update(Keyboard.GetState().GetPressedKeys());
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.TransparentBlack);
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            if (Keyboard.GetState().IsKeyDown(Keys.LeftShift))
+                spriteBatch.Draw(character.FocusImg, character.Position, character.GetFocusRectangle(), Color.White, character.Angle, character.GetFocusCenter(), 1.0f, SpriteEffects.None, 1);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
